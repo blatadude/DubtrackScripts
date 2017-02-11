4f9d113cf22a878f40ccff5cd5c7529593254597
Chr0nX <chr0nx@live.de>
2017-02-11 15:53:30 +0100

    new dubx script (load dubx from new repo)

---
+++ NewDubX.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         [Dubtrack] Load new DubX
+// @namespace    Chr0nX/Dubtrack/DubX
+// @version      0.1
+// @description  Loads DubX from the new repo.
+// @author       Chr0nX
+// @match        https://*.dubtrack.fm/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $.getScript('https://rawgit.com/coryshaw1/DubX-Script/master/beta.js');
+    $('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/coryshaw1/DubX-Script/master/css/asset.css">');
+})();
