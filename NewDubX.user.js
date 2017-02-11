7d8dc9354ebaf8f64549dfa845f439ded80afcc2
Chr0nX <chr0nx@live.de>
2017-02-11 16:52:00 +0100

    new dubx script: load css for 'dub info on hover' option

--- NewDubX.user.js
+++ NewDubX.user.js
@@ -16,6 +16,7 @@
             $.getScript('https://rawgit.com/coryshaw1/DubX-Script/master/beta.js');
             $('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/coryshaw1/DubX-Script/master/css/asset.css">');
             $('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/coryshaw1/DubX-Script/master/css/options/autocomplete.css">');
+            $('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/coryshaw1/DubX-Script/master/css/options/dubinfo.css">');
             $(document).on('DOMNodeInserted', '.header-right-navigation', function () {
                 $('.header-right-navigation .for img').attr('src', 'https://rawgit.com/coryshaw1/DubX-Script/master/params/params.svg');
             });
