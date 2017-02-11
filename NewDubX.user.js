85ffec3c25edfa5bed6d4df7e4a4210642412a49
Chr0nX <chr0nx@live.de>
2017-02-11 16:47:06 +0100

    new dubx script: load correct icon

--- NewDubX.user.js
+++ NewDubX.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Dubtrack] Load new DubX
 // @namespace    Chr0nX/Dubtrack/DubX
-// @version      0.1.1
+// @version      0.1.2
 // @description  Loads DubX from the new repo.
 // @author       Chr0nX
 // @match        https://*.dubtrack.fm/*
@@ -16,6 +16,9 @@
             $.getScript('https://rawgit.com/coryshaw1/DubX-Script/master/beta.js');
             $('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/coryshaw1/DubX-Script/master/css/asset.css">');
             $('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/coryshaw1/DubX-Script/master/css/options/autocomplete.css">');
+            $(document).on('DOMNodeInserted', '.header-right-navigation', function () {
+                $('.header-right-navigation .for img').attr('src', 'https://rawgit.com/coryshaw1/DubX-Script/master/params/params.svg');
+            });
         }, 2000);
     });
 })();
